cef54e56869a9fe1ce958aa2f64473e04948592c
Teodora Budeanu <teo.budeanu@gmail.com>
2018-12-28 00:11:04 +0200

    m

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import Exceptions.IdValidationException;
+import Exceptions.NameValidationException;
+import Exceptions.ShootingRangeValidationException;
+import Exceptions.TimeValidationException;
+
 import java.text.ParseException;
 
 public class Main {
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) throws ParseException, NameValidationException, IdValidationException, TimeValidationException, ShootingRangeValidationException {
 
         FileProcessor f = new FileProcessor();
         f.parseAthletes();
